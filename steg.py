22f48de4cd212bcc91a29abc0c77fa65e1b5ec2c
Zibran Khan <zibrangni@gmail.com>
2017-07-15 17:12:08 +0000

    Delete steg.py

--- steg.py
+++
@@ -1,5 +0,0 @@
-from steganography.steganography import Steganography
-
-Steganography.encode(/root/Desktop/mm.jpeg,/root/Desktop/aa.jpeg,'hi i am zibran')
-
-secret_text = Steganography.decode('/root/Desktop/aa.jpeg')
