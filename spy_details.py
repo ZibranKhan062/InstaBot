fb1acd35b3359b13ff60dc92172515a2b802b85e
Zibran Khan <zibrangni@gmail.com>
2017-07-15 17:11:48 +0000

    Delete spy_details.py

--- spy_details.py
+++
@@ -1,8 +0,0 @@
-spy_name=raw_input('Enter Spy name:')
-
-spy_age=int(raw_input('Enter Spy age:'))
-spy_salutation=raw_input('Enter Spy Salutation:')
-spy_rating=float(raw_input('Enter Spy Rating:'))
-spy_is_online=True
-
-
